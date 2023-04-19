4c1318da2ec7e94d5d307945ab9f44b6ef9e5b3a
xxqstall <yaroslavfurmanov2019@gmail.com>
2023-04-19 15:23:27 +0200

    upfd

--- AccTrade/View/LoginScreen.xaml.cs
+++ AccTrade/View/LoginScreen.xaml.cs
@@ -20,27 +20,7 @@ namespace AccTrade.View
 
         private void loginbtn_Click(object sender, RoutedEventArgs e)
         {
-            if (logtb.Text != "" && passtb.Password != "")
-            {
-                using (AppContext db = new AppContext())
-                {
-                    string username = logtb.Text;
-                    string password = md5.hashPassword(passtb.Password);
-                    var user = db.Logins.Where((u) => u.Username == username && u.Password == password).FirstOrDefault();
-
-                    if (user != null)
-                    {
-                        MainWindow main = new MainWindow();
-                        Close();
-                        main.Show();
-                    }
-                    else MessageBox.Show("Неверные данные!");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Введите все данные!");
-            }
+            
         }
     }
 }
