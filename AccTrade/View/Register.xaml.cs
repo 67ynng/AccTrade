268de6fdbe0b787fc748dbfb1b4f285f57fc0b3c
xxqstall <yaroslavfurmanov2019@gmail.com>
2023-04-19 15:24:58 +0200

    add pass hash

--- AccTrade/View/Register.xaml.cs
+++ AccTrade/View/Register.xaml.cs
@@ -17,7 +17,56 @@ namespace AccTrade.View
 
         private void loginbtn_Click(object sender, RoutedEventArgs e)
         {
-            
+            new Thread(() =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    string username = logtb.Text;
+                    string email = emailtb.Text;
+                    string password = md5.hashPassword(passtb.Password);
+
+
+                    if (email == "" || password == "" || username == "")
+                        MessageBox.Show("All fields must be filled!");
+                    else if (!Regex.IsMatch(email, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+                        MessageBox.Show("Wrong mail format!");
+                    else if (email.Length < 13)
+                        MessageBox.Show("Email length cannot be less than 13 characters!");
+                    else
+                    {
+                        using (AppContext db = new AppContext())
+                        {
+                            db.Database.EnsureCreated();
+                            bool isExists = db.Logins.Any(value => value.Email == email);
+                            bool isExists2 = db.Logins.Any(value => value.Username == username);
+
+                            if (isExists)
+                                MessageBox.Show("This mail is already in the database!");
+                            else if (isExists2)
+                                MessageBox.Show("This username is already in the database!");
+                            else
+                            {
+                                var user = new Login
+                                {
+                                    Username = username,
+                                    Email = email,
+                                    Password = password
+
+                                };
+
+                                db.AddRange(user);
+                                db.SaveChanges();
+                                MessageBox.Show("Registration completed successfully!");
+                                LoginScreen log = new LoginScreen();
+                                Close();
+                                log.Show();
+                            }
+                        }
+                    }
+                });
+            })
+            {
+            }.Start();
         }
     }
 }
