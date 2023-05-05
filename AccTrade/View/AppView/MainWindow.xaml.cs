13dc73718b6f393ec3b383a886f8b9ae24ed715d
xxqstall <yaroslavfurmanov2019@gmail.com>
2023-05-05 03:24:22 +0200

    design

--- AccTrade/View/AppView/MainWindow.xaml.cs
+++ AccTrade/View/AppView/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.Windows;  
 namespace AccTrade.View;
 using AccTrade.View.AppView;
-
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 public partial class MainWindow : Window
 {
@@ -13,6 +15,7 @@ public partial class MainWindow : Window
     private void Add_btn_Click(object sender, RoutedEventArgs e)
     {
         MyFrame.Navigate(new AddScreen());
+
     }
 
     private void MainScreen_btn_Click(object sender, RoutedEventArgs e)
